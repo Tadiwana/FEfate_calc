fc5d1bb0d94b16cf8eb5ee58da59d7fd016d035b
OvaOne <MissionBit@Missions-MacBook-Air-2.local>
2016-03-22 17:55:34 -0700

    Total war

--- totalWar/urls.py
+++ totalWar/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 
 urlpatterns = [
 
-    url(r'^', 'totalWar.views.homepageTW', name='homepageTW'),
+    url(r'^$', 'totalWar.views.homepageTW', name='homepageTW'),
+    url(r'^GreatBritian/', 'totalWar.views.GreatBritian', name='GreatBritian')
 
 ]
--- totalWar/views.py
+++ totalWar/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepageTW(request):
     return render(request, "homepageTW.html", {})
+
+def GreatBritian(request):
+    return render(request, "GreatBritian.html", {})
