dd5fbe6140224638b3fc443f91f8c3f8414df4f9
sam <sam442000@gmail>
2016-04-08 23:34:14 -0700

    added a button and changed some names
    the button currently doesn’t do anything

--- data/models.py
+++ data/models.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-from adaptor.model import CsvDbModel
 from django.db import models
 
 
@@ -36,8 +35,28 @@ class Adult(models.Model):
     classP = models.CharField(max_length=30)
     class2 = models.CharField(max_length=30)
 
-class AdultcsvModel(CsvDbModel):
+class Child(models.Model):
 
-   class Meta:
-        dbModel = Adult
-        delimiter=str(',')
+    name = models.CharField(max_length=20)
+    nonvariableParent = models.CharField(max_length=20)
+
+    hpG = models.CharField(max_length=10)
+    strG = models.CharField(max_length=10)
+    magG = models.CharField(max_length=10)
+    skillG = models.CharField(max_length=10)
+    spdG = models.CharField(max_length=10)
+    lckG = models.CharField(max_length=10)
+    defG = models.CharField(max_length=10)
+    resG = models.CharField(max_length=10)
+
+    strB = models.CharField(max_length=10)
+    magB = models.CharField(max_length=10)
+    skillB = models.CharField(max_length=10)
+    spdB = models.CharField(max_length=10)
+    lckB = models.CharField(max_length=10)
+    defB = models.CharField(max_length=10)
+    resB = models.CharField(max_length=10)
+
+    classP = models.CharField(max_length=30)
+    class2 = models.CharField(max_length=30)
+    class3 = models.CharField(max_length=30)
