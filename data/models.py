0b54de39e77824daa1329270489cd31be1422366
sam <sam442000@gmail>
2016-04-17 16:52:13 -0700

    made the website work
    too bad it displays incorrect information, but at least we have
    something. it can do the math correctly, we just need to change the
    data for the adults.

--- data/models.py
+++ data/models.py
@@ -35,6 +35,7 @@ class Adult(models.Model):
     classP = models.CharField(max_length=30)
     class2 = models.CharField(max_length=30)
 
+
 class Child(models.Model):
 
     name = models.CharField(max_length=20)
@@ -49,14 +50,29 @@ class Child(models.Model):
     defG = models.CharField(max_length=10)
     resG = models.CharField(max_length=10)
 
-    strB = models.CharField(max_length=10)
-    magB = models.CharField(max_length=10)
-    skillB = models.CharField(max_length=10)
-    spdB = models.CharField(max_length=10)
-    lckB = models.CharField(max_length=10)
-    defB = models.CharField(max_length=10)
-    resB = models.CharField(max_length=10)
+    classP = models.CharField(max_length=30)
+    class2 = models.CharField(max_length=30)
+
+class newChild(models.Model):
+    name = models.CharField(max_length=20)
+    nonvariableParent = models.CharField(max_length=20)
+
+    hpG = models.DecimalField(max_digits=10,decimal_places=2)
+    strG = models.DecimalField(max_digits=10,decimal_places=2)
+    magG = models.DecimalField(max_digits=10,decimal_places=2)
+    skillG = models.DecimalField(max_digits=10,decimal_places=2)
+    spdG = models.DecimalField(max_digits=10,decimal_places=2)
+    lckG = models.DecimalField(max_digits=10,decimal_places=2)
+    defG = models.DecimalField(max_digits=10,decimal_places=2)
+    resG = models.DecimalField(max_digits=10, decimal_places=2)
+
+    strB = models.IntegerField()
+    magB = models.IntegerField()
+    skillB = models.IntegerField()
+    spdB = models.IntegerField()
+    lckB = models.IntegerField()
+    defB = models.IntegerField()
+    resB = models.IntegerField()
 
     classP = models.CharField(max_length=30)
     class2 = models.CharField(max_length=30)
-    class3 = models.CharField(max_length=30)
--- data/views.py
+++ data/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from data.models import Adult
+from data.models import Adult, Child, newChild
 from django import forms
+from decimal import *
 # Create your views here.
 
 def homepageFE(request):
@@ -21,8 +22,27 @@ def calc(request):
 def home(request):
     return render(request, "home.html", {})
 
-# def calculate(mother, father):
-
+def calculate(mother, father):
+    mylist=[father.name, (Decimal(mother.hpG) + Decimal(father.hpG))/2, (Decimal(mother.strG) + Decimal(father.strG))/2,(Decimal(mother.magG) + Decimal(father.magG))/2,(Decimal(mother.skillG) + Decimal(father.skillG))/2,(Decimal(mother.spdG) + Decimal(father.spdG))/2, (Decimal(mother.lckG) + Decimal(father.lckG))/2, (Decimal(mother.defG) + Decimal(father.defG))/2, (Decimal(mother.resG) + Decimal(father.resG))/2, (int(mother.strB) + int(father.strB) + 1),(int(mother.magB) + int(father.magB) + 1),(int(mother.skillB) + int(father.skillB) + 1), (int(mother.spdB) + int(father.spdB) + 1),(int(mother.lckB) + int(father.lckB) + 1),(int(mother.defB) + int(father.defB) + 1),(int(mother.resB) + int(father.resB) + 1)]
+    # HPg = (Decimal(mother.hpG) + Decimal(father.hpG))/2
+    # STRg=
+    # MAGg=
+    # SKLg=
+    # SPDg=
+    # LCKg=
+    # DEFg=
+    # RESg=
+    # STRb=
+    # MAGb=
+    # SKLb=
+    # SPDb=
+    # LCKb=
+    # DEFb=
+    # cp=
+    # c2=
+    # c=newChild(name = "something", nonvariableParent = father.name , hpG = HPg, strG = STRg,magG = MAGg, skillG =SKLg, spdG = SPDg, lckG =LCKg, defG=DEFg, resG=RESg, strB = STRb, magB= MAGb, skillB=SKLb, spdB = SPDb, lckB = LCKb, defB = DEFb, resB = RESb,  classP =cp, class2 = c2))
+    # c.save()
+    return mylist
 def child(request):
     form = ChildForm(request.GET)
     form.is_valid()
@@ -30,10 +50,11 @@ def child(request):
     father_id = form.cleaned_data['father']
     mother = Adult.objects.filter(id=mother_id).first()
     father = Adult.objects.filter(id=father_id).first()
-   # child = calculat(mother=mother, father=father)
+
+    child = calculate(mother=mother, father=father)
 
     print(str(form.cleaned_data))
-    return render(request, "child.html", {"objects": Adult.objects.all(), child: child})
+    return render(request, "child.html", {"objects": Adult.objects.all(), "child": child})
 
 class ChildForm(forms.Form):
     father = forms.ChoiceField(label="father", choices=[ (adult.id, adult.name) for adult in Adult.objects.filter(sex = "M")])
